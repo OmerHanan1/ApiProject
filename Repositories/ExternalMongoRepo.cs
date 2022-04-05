f3ff6c1f648fa455a0ac0230b7714f924cf4c326
Omer Hanan <t-omerhanan@microsoft.com>
2022-04-05 20:10:02 +0300

    updating master to local memory repo

---
+++ Repositories/ExternalMongoRepo.cs
@@ -0,0 +1,68 @@
+﻿using ApiProject.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace ApiProject.Repositories
+{
+    /// <summary>
+    /// Extrenal memory option, relying ontop docker.
+    /// </summary>
+    public class ExternalMongoRepo : ILocalAndExternalRepo
+    {
+        private const string RepositoryName = "MongoDbApiRepo";
+        private const string CollectionName = "MongoDbApiCollection";
+        private readonly IMongoCollection<Item> _repository;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="mongoClient"></param>
+        public ExternalMongoRepo(IMongoClient mongoClient) 
+        {
+            IMongoDatabase databse = mongoClient.GetDatabase(RepositoryName);
+            _repository = databse.GetCollection<Item>(CollectionName);
+        }
+
+        public void CreateNewItem(Item item)
+        {
+            _repository.InsertOne(item);
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            FilterDefinition<Item> filters = Builders<Item>.Filter.Eq("Id", id);
+            _repository.DeleteOne(filters);
+        }
+
+        public Item GetItem(Guid id)
+        {
+            var temp = GetItems();
+            foreach (var item in temp) 
+            {
+                if (item.Id == id) 
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            return _repository.Find(new BsonDocument()).ToList();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            FilterDefinition<Item> filter = Builders<Item>.Filter.Eq("Id", item.Id);
+            UpdateDefinition<Item> updateName = Builders<Item>.Update.AddToSet<string>("Name", item.Name);
+            UpdateDefinition<Item> updateDescription = Builders<Item>.Update.AddToSet<string>("Description", item.Description);
+
+            _repository.UpdateOne(filter, updateName);
+            _repository.UpdateOne(filter, updateDescription);
+
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +32,14 @@ namespace ApiProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            //services.AddSingleton<IMongoClient>(ServiceProvider =>
+            //{
+                //var settings = Configuration.GetSection(nameof(MongoDBSettings)).Get<MongoDatabaseSettings>();
+                //return new MongoClient(settings.ConnectionString);
+            //}
+            //);
+
             services.AddSingleton<ILocalAndExternalRepo, LocalMemoryItemsRepo>(); // Register the dependency
             services.AddControllers();
             services.AddSwaggerGen(c =>
